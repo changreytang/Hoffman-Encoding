cac7d243d96e6470bf236ac056aeabed18355872
Rey Tang <tang.changrey@gmail.com>
2016-02-05 23:56:33 +0000

    Started testing implementation through Main

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <cstdio>
+
+#include "MinHeap.h"
 using namespace std;
 
 int main() {
     char ch;
     freopen("test.txt","r",stdin);
     int* array = new int[27];
+    // Insert characters by ASCII values into array of size 27
     while (cin >> noskipws >> ch) {
         if((int)(ch - 97) >= 0 && (int)(ch - 97) <= 25)
             array[(int)(ch - 97)]++;
+        // Insert spaces at the end of the array *ASCII value not in same range
         else if((int)(ch - 97) == -65)
             array[26]++;
     }
-    for (int i=0; i < 27; i++) {
-        cout << array[i] << endl;
-    }
+    MinHeap h(array);
+    h.print();
     
     return 0;
 }
