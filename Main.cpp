519e55ffa7353b754ee93ffb5f8b9f87c5606aec
Rey Tang <tang.changrey@gmail.com>
2016-02-06 19:59:00 +0000

    Added functionality for command line arguments

--- Main.cpp
+++ Main.cpp
@@ -1,23 +1,52 @@
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 
 #include "MinHeap.h"
+#include "Node.h"
+#include "Encode.h"
 using namespace std;
 
-int main() {
-    char ch;
-    freopen("test.txt","r",stdin);
-    int* array = new int[27];
-    // Insert characters by ASCII values into array of size 27
-    while (cin >> noskipws >> ch) {
-        if((int)(ch - 97) >= 0 && (int)(ch - 97) <= 25)
-            array[(int)(ch - 97)]++;
-        // Insert spaces at the end of the array *ASCII value not in same range
-        else if((int)(ch - 97) == -65)
-            array[26]++;
+int main(int argc, char *argv[]) {
+    if (argc != 3) { 
+        cerr << "Usage: "<< argv[0] << " encode/decode" << " TextFileName" << endl; 
+        exit(1);
     }
-    MinHeap h(array);
-    h.print();
+    if (strcmp(argv[1],"encode") == 0) {
+        char ch;
+        freopen(argv[2],"r",stdin);
+        int* array = new int[27];
+        // Insert characters by ASCII values into array of size 27
+        while (cin >> noskipws >> ch) {
+            if((int)(ch - 97) >= 0 && (int)(ch - 97) <= 25)
+                array[(int)(ch - 97)]++;
+            // Insert spaces at the end of the array *ASCII value not in same range
+            else if((int)(ch - 97) == -65)
+                array[26]++;
+        }
+        MinHeap h(array);
+        Encode huffman(h);
+        int arr[30];
+        huffman.encodeCharacters(huffman.getRoot(), arr, 0);
+        fclose(stdin);
+        cin.clear();
+        freopen(argv[2],"r",stdin);
+        while (cin >> noskipws >> ch) {
+            huffman.printHuffmanCode(ch);
+        }
+        cout << endl;
+        fclose(stdin);
+        cin.clear();
+    }
+    else if (strcmp(argv[1], "decode") == 0) {
+        string code;
+        freopen(argv[2],"r",stdin);
+        
+    }
+    else {
+        cerr << "Usage: "<< argv[0] << " encode/decode" << " TextFileName" << endl; 
+        exit(1);
+    }
     
     return 0;
 }
