f7b1d29393f2cad537462e439827ffcf7cbe6e06
Rey Tang <tang.changrey@gmail.com>
2016-02-06 00:32:10 +0000

    Tested and implemented insert and deleteMin

--- MinHeap.cpp
+++ MinHeap.cpp
@@ -48,7 +48,7 @@ void MinHeap::percolateDown(int index) {
         // Check if there is a right child that is less than or equal to value of left child
         if(child_index < (size - 1) && heap_array[child_index + 1]->getCount() <= heap_array[child_index]->getCount())
             child_index++;
-        // Check if the child is actually less than the parent value
+        // Check if the child is actually less than the parent and swap if true 
         if(heap_array[child_index]->getCount() < heap_array[index]->getCount()) {
             heap_array[index] = heap_array[child_index];
             heap_array[child_index] = temp;
@@ -58,21 +58,40 @@ void MinHeap::percolateDown(int index) {
     }
 }
 
+void MinHeap::percolateUp(int index) {
+    int parent_index;
+    Node* temp = heap_array[index];
+    for( ; index/2 > 0; index = parent_index) {
+        parent_index = index/2;
+        // Check if the parent is greater than child and swap if true
+        if(heap_array[parent_index]->getCount() > heap_array[index]->getCount()) {
+            heap_array[index] = heap_array[parent_index];
+            heap_array[parent_index] = temp;
+        } else {
+            break;
+        }
+    }
+}
+
+
+// I can assume this Heap will never be used greater than size
 void MinHeap::insert(Node* n) {
-    
+    size++;
+    heap_array[size - 1] = n;
+    percolateUp(size - 1);
+}
+
+Node* MinHeap::deleteMin() {
+    Node* temp = heap_array[1];
+    heap_array[1] = heap_array[size - 1];
+    heap_array[size - 1] = NULL;
+    size--;
+    percolateDown(1);
+    return temp;
 }
 
 void MinHeap::print() {
     for(int i = 1; i < size; i++ ) {
         cout << heap_array[i]->getCount() << " " << heap_array[i]->getCharacter() << endl;
     }
-}
-
-Node* MinHeap::deleteMin() {
-    Node* temp = heap_array[1];
-    heap_array[1] = heap_array[size - 1];
-    delete heap_array[size - 1];
-    --size;
-    percolateDown(1);
-    return temp;
 }
