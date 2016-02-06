cd0bb31a95100c0b77defaf3d63d4919bd15f21e
Rey Tang <tang.changrey@gmail.com>
2016-02-06 10:06:03 +0000

    Implemented the creation of finished Hoffman trie

--- Encode.cpp
+++ Encode.cpp
@@ -1,12 +1,33 @@
 #include <cstdlib>
+#include <iostream>
 #include "Node.h"
 #include "MinHeap.h"
 #include "Encode.h"
 
+using namespace std;
+
 Encode::Encode() {
-    
+    root = NULL;
+    huff_codes = NULL;
 }
 
 Encode::Encode(MinHeap heap) {
-    this->heap = heap;
+    while(heap.getSize() > 2) {
+        Node* n1 = heap.deleteMin();
+        Node* n2 = heap.deleteMin();
+        char c = '#';
+        Node* tri_node = new Node(c, n1->getCount()+n2->getCount(), n2, n1);
+        heap.insert(tri_node);
+    }
+    this->root = heap.getRoot();
+    this->huff_codes = new string[27];
+}
+
+void Encode::printLeaves(Node* root) {
+    if(root == NULL)       
+        return;
+    if(root->getLeft() == NULL && root->getRight() == NULL)      
+        cout << root->getCount() << " " << root->getCharacter() << endl;
+    printLeaves(root->getLeft()); 
+    printLeaves(root->getRight());      
 }
--- Encode.h
+++ Encode.h
@@ -2,17 +2,24 @@
 #define ENCODE_H
 
 #include <cstdlib>
+#include <string>
 #include "Node.h"
 #include "MinHeap.h"
 
 class Encode {
 private:
-    MinHeap heap;
     Node* root;
+    string* huff_codes;
+    string current_code = "";
+    
+    void encodeCharacters(Node* root);
     
 public:
     Encode();
     Encode(MinHeap heap);
+    
+    void printLeaves(Node* root);
+    Node* getRoot() { return this->root; }
 
 };
 
