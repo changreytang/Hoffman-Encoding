230edcdf547a973793106846517ab9abfd9bae31
Rey Tang <tang.changrey@gmail.com>
2016-02-07 04:17:22 +0000

    Finished encode and decoding

--- Main.cpp
+++ Main.cpp
@@ -7,46 +7,38 @@
 #include "Encode.h"
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) { 
-        cerr << "Usage: "<< argv[0] << " encode/decode" << " TextFileName" << endl; 
-        exit(1);
+int main() {
+    
+    char ch;
+    freopen("plain.input","r",stdin);
+    int* array = new int[27];
+    // Insert characters by ASCII values into array of size 27
+    while (cin >> noskipws >> ch) {
+        if((int)(ch - 97) >= 0 && (int)(ch - 97) <= 25)
+            array[(int)(ch - 97)]++;
+        // Insert spaces at the end of the array *ASCII value not in same range
+        else if((int)(ch - 97) == -65)
+            array[26]++;
     }
-    if (strcmp(argv[1],"encode") == 0) {
-        char ch;
-        freopen(argv[2],"r",stdin);
-        int* array = new int[27];
-        // Insert characters by ASCII values into array of size 27
-        while (cin >> noskipws >> ch) {
-            if((int)(ch - 97) >= 0 && (int)(ch - 97) <= 25)
-                array[(int)(ch - 97)]++;
-            // Insert spaces at the end of the array *ASCII value not in same range
-            else if((int)(ch - 97) == -65)
-                array[26]++;
-        }
-        MinHeap h(array);
-        Encode huffman(h);
-        int arr[30];
-        huffman.encodeCharacters(huffman.getRoot(), arr, 0);
-        fclose(stdin);
-        cin.clear();
-        freopen(argv[2],"r",stdin);
-        while (cin >> noskipws >> ch) {
-            huffman.printHuffmanCode(ch);
-        }
-        cout << endl;
-        fclose(stdin);
-        cin.clear();
+    MinHeap h(array);
+    Encode huffman(h);
+    int arr[30];
+    huffman.encodeCharacters(huffman.getRoot(), arr, 0);
+    fclose(stdin);
+    cin.clear();
+    freopen("plain.input","r",stdin);
+    while (cin >> noskipws >> ch) {
+        huffman.printHuffmanCode(ch);
     }
-    else if (strcmp(argv[1], "decode") == 0) {
-        string code;
-        freopen(argv[2],"r",stdin);
-        
+    cout << endl;
+    fclose(stdin);
+    cin.clear();
+    freopen("encode.input","r",stdin);
+    string code;
+    while (cin >> code) {
+        huffman.decodeCharacters(huffman.getRoot(), code);
     }
-    else {
-        cerr << "Usage: "<< argv[0] << " encode/decode" << " TextFileName" << endl; 
-        exit(1);
-    }
+    cout << endl;
     
     return 0;
 }
